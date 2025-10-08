0e80b44ee64808a7367087893a56364cd644d508
Abdullah742664 <158731180+Abdullah742664@users.noreply.github.com>
2025-10-08 16:30:25 +0600

    Revert "fix"
    This reverts commit 3c20098201ca351159f6a3d1327011f75276256a.

--- utils/socket.js
+++ utils/socket.js
@@ -92,24 +92,15 @@ async function sendPushNotificationToOfflineUsers(chatId, senderId, senderFirstN
       }
 
       if (user.fcmToken) {
-        let notificationTitle;
-        let notificationBody;
-
-        if (chat.isGroup) {
-          // Group notification
-          notificationTitle = chat.name; // Group name
-          notificationBody = `${senderFirstName} ${senderLastName}: ${messageContent}`; // Sender's name and message
-        } else {
-          // Private notification
-          notificationTitle = `${senderFirstName} ${senderLastName}`; // Sender's name
-          notificationBody = messageContent; // Message content only
-        }
+        const notificationTitle = chat.isGroup
+          ? chat.name // Use group name if it's a group chat
+          : `${senderFirstName} ${senderLastName}`; // Use sender's name otherwise
 
         const notificationPayload = {
           token: user.fcmToken,
           notification: {
             title: notificationTitle,
-            body: notificationBody,
+            body: `${senderFirstName} ${senderLastName}: ${messageContent}`, // Include sender's name in the body
           },
           data: {
             chatId: String(chatId),
